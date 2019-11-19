4153f75b71451e3da622a6b6b4fbfd3a5c0184a5
Nahid Hasan <37169055+rajunahidhasan0@users.noreply.github.com>
2019-11-19 13:19:33 +0600

    Add files via upload

--- charTypeSwapping.asm
+++ charTypeSwapping.asm
@@ -1,39 +1,51 @@
 .MODEL SMALL
 .STACK 100H
 .CODE
-MAIN PROC    
-            ;char input    
-MOV AH, 1
-INT 21H     
-         
-MOV AL,BL       
 
-            ;Newline print
-MOV AH,2
+MAIN PROC
+
+    ; 1 will request input
+    
+MOV AH,1
+INT 21H  
+    ; storing input into temporary register
+MOV BL,AL
+
+    ; converting upper to lower and vice-versa
+
+CMP BL,'z'
+    ; ignoring spacial charecter
+JG ignore  
+CMP BL,'A'
+JL ignore
+
+
+CMP BL,96
+JG  upper
+
+CMP BL,90
+JG  ignore
+
+ADD BL,64
+upper:
+SUB BL,32
+         
+ignore:
+    ;ADD DL,-10
+    ; PRINTING NEW LINE (actually printing 80 chars)
+    ; setting for output request
+MOV AH,2 
 MOV DL,0AH
 INT 21H
 MOV DL,0DH
-INT 21H 
- 
-;MOV BL,AL   
-            ;swapping (capital, small)
+INT 21H
 
-CMP BL,91
-JG Line1 
-
-ADD BL,64
-Line1:
-SUB BL,32
-
-            ;output print
-MOV DL,AL
+MOV DL,BL
 MOV AH,2
-INT 21H     
-            ;this 2 line maybe not necessary            
-MOV AH, 4CH
-INT 21H     
+INT 21H
 
-MAIN ENDP     
+MOV AH,4CH  ;eat extra printer char
+INT 21H
 
+MAIN ENDP
 END MAIN
-        `
